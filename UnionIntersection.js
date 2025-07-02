1740e46390de80561c03146bb6e01fe9da1a45f8
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-07-02 16:03:16 -0700

    practice in UnionIntersection.ts file

--- UnionIntersection.js
+++ UnionIntersection.js
@@ -1,1 +1,7 @@
 console.log("Hello");
+console.log("Worlds");
+function Login(User) {
+    return User;
+}
+console.log(Login({ Name: "ali", role: "Admin" }));
+console.log(Login({ username: "shahzaib", Purchase: 3000 }));
--- UnionIntersection.ts
+++ UnionIntersection.ts
@@ -1,1 +1,23 @@
 console.log("Hello")
+console.log("Worlds")
+// command to change the file name:     alias union="npx ts-node UnionIntersection.ts"
+
+// Union and intersection type 
+//    ||          task       ||         //
+
+interface Admin{
+    Name:string;
+    role:"Admin"
+}
+
+interface Costumer{
+    username:string;
+    Purchase:number;
+}
+
+  function Login(User:Admin | Costumer){
+  
+    return User;
+  }      
+  console.log(Login({Name:"ali",role:"Admin"}));
+  console.log(Login({username:"shahzaib",Purchase:3000}))
