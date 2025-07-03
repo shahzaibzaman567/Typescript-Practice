9f7a2b58d66b98dadc03c51cf2668cd173a438b3
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-07-02 18:01:59 -0700

    practice with TS topics

--- UnionIntersection.ts
+++ UnionIntersection.ts
@@ -23,7 +23,7 @@ console.log(Login({ Name: "ali", role: "Admin" }));
 console.log(Login({ username: "shahzaib", Purchase: 3000 }))
 
 //              task   2         //
-
+//Intersection
 
 
 
@@ -48,10 +48,112 @@ return  Super
 console.log(Intersection({Name:"ali",role:"Admin",username:"shah",Purchase:200}) )
 
 
+//what  is Auto Merging ?  
+//Auto merging can use in interface only the interface was declare but use all properties
+//this is called overloading
+
+interface Student{
+    Name:string;
+}
+
+interface Student{
+    Class:string ;
+}
+interface Student{
+    RollNo:number;
+
+}
+
+const St1:Student={
+    Name:"shahzaib",
+    Class: 9 +"th",
+    RollNo:804
+}
 
 
 
 
+console.log(St1);
+
+//            ||          Task with Enum        ||    //
+//what is enum to we use to creat a group of we kam a constant valu  
+
+
+enum Role {
+    Student = "Student",
+    Teacher = "Teacher",
+    Principal = "Principal"
+}
+
+function School(role:Role){
+
+
+    if(Role.Student === role){
+console.log("📚 Homework karo...");
+    }
+   else if(Role.Teacher===role){
+        console.log("🧑‍🏫 Students ko parhao...");
+    }
+    else if(Role.Principal===role){
+        console.log("🎓 School manage karo...");
+    }
+    else{
+ throw new Error("❌ Error: Role not found");
+    }
+}
+School(Role.Student);
+//with                  chat GBT
+
+// enum Role {
+//     Student = "Student",
+//     Teacher = "Teacher",
+//     Principal = "Principal"
+// }
+
+// function School(role: Role) {
+//     if (Role.Student === role) {
+//         console.log("📚 Homework karo...");
+//     } else if (Role.Teacher === role) {
+//         console.log("🧑‍🏫 Students ko parhao...");
+//     } else if (Role.Principal === role) {
+//         console.log("🎓 School manage karo...");
+//     } else {
+//         throw new Error("❌ Error: Role not found");
+//     }
+// }
+
+// School(Role.Student); // ✅ Correct call
+
+
+//                  task                //
+//creat a person interface  with key:fullame: string ,age:number, gender:Enum
+
+
+
+enum gender{
+    Male="Male",
+    Female="Female",
+    Other="Other",
+}
+interface Per {
+    Name:string;
+    Age:number;
+    gender:gender;
+
+}
+
+const P1:Per={
+    Name:"shahzaib",
+    Age:15,
+    gender:gender.Male
+}
+const P2:Per={
+    Name:"fatima",
+    Age:40,
+    gender:gender.Female
+}
+console.log(P1)
+console.log(P2)
 
 
 
